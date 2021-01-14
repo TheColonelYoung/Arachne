848ab55322cc76fc2ecb54aeced65343a6018890
Petr Malanik <Malanik.Petr.gm@gmail.com>
2021-01-14 09:55:44 +0100

    FW: project cleanup

---
+++ fw/build/tasker_event.d
@@ -0,0 +1,6 @@
+build/tasker_event.o: alohal/tools/tasker_event.cpp \
+ alohal/tools/tasker_event.hpp alohal/misc/invocation_wrapper.hpp
+
+alohal/tools/tasker_event.hpp:
+
+alohal/misc/invocation_wrapper.hpp:
